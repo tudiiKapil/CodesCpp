bb36ac703038a92030f88c9e40dd83a8d5e115f1
tudiiKapil <kapilavinash42@gmail.com>
2019-02-12 20:33:30 +0530

    ye lo

---
+++ reverse.cpp
@@ -0,0 +1,13 @@
+# include <bits/stdc++.h>
+using namespace std ;
+int main()
+{
+	int a;
+	cout <<"Enter a value: ";
+	cin >> a;
+
+	for(int i = a; i != 0; i--){
+	cout << i <<"\n";
+    }
+   return 0;
+}
